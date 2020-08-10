f80193b16b492d969012dd725499a4a92e6679d0
Yudith <yudith@MacBook-Air-de-administrador.local>
2020-08-10 17:04:34 -0500

    unit test getdata getalldata

--- controller/__tests__/sql_functions.spec.js
+++ controller/__tests__/sql_functions.spec.js
@@ -1,5 +1,7 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-console */
 // Importar sinon
+jest.setTimeout(1000000);
 const sinon = require('sinon');
 // Mockear MySQL con sinon
 const mockMysql = sinon.mock(require('mysql'));
@@ -12,66 +14,114 @@ const connectStub = sinon.stub().callsFake((cb) => {
 // Crear stub para query
 const queryStub = sinon.stub();
 // Crear stub para end
-const endStub = sinon.stub();
+// const endStub = sinon.stub();
 // Cuando se cree la conexión, reemplazar resultado con stubs
 mockMysql.expects('createConnection').returns({
   connect: connectStub,
   query: queryStub,
-  end: endStub,
+  // end: endStub,
 });
+
 // Invocar nuestra libreria/metodos a probar
 const db = require('../../db-data/sql_functions');
 
-describe('mySQL', () => {
-  /* it('should create a db', (done) => {
-    // En cada test podemos reemplazar la funcion a ser llamada
-    // usando callsFake
-    expect.hasAssertions();
-    queryStub.callsFake((query, cb) => {
-      expect(query).toBe('CREATE DATABASE mydb');
-      // Este callback es el que se espera sea llamado en cada query
-      // aqui podemos retornar cualquier información que deseamos
-      cb();
-      done();
-    });
-    db.createDb();
-  }); */
+const userTable = [
+  {
+    _id: 1010001,
+    email: 'test@test.test',
+    password: 'password12345',
+  }];
+const error = new Error('error');
 
-  /* it('should create a table', (done) => {
-    expect.hasAssertions();
+describe('getAllData(user)', () => {
+  // get all data
+  it('should throw error if data is null', () => {
+    // en vez de llamar .query llama al callFake
     queryStub.callsFake((query, cb) => {
-      expect(query).toBe('CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))');
-      cb();
-      done();
+      cb(error, []);
     });
-    db.createTable();
-  }); */
-  it('should insert into a table', (done) => {
-    expect.hasAssertions();
-    queryStub.callsFake((query, cb) => {
-      console.log(query);
-      expect(query).toBe("INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')");
-      cb();
-      done();
-    });
-    db.postData('users', { email: 'test@cucaracha.test', password: '123456', rolesAdmin: false });
+    return db.getAllData('users')
+      .catch((error) => {
+        expect(error.message).toBe('error');
+      });
   });
-  it('should select from a table', (done) => {
-    expect.hasAssertions();
+
+  it('should return data if exits from users', () => {
+    // en vez de llamar .query llama al callFake
     queryStub.callsFake((query, cb) => {
-      expect(query).toBe('SELECT * FROM customers');
-      cb(null, 'test result');
-      done();
+      cb('error', userTable);
     });
-    db.selectFromTable();
+    return db.getAllData('users')
+      .then((result) => {
+        result.map((result) => {
+          expect(typeof result._id).toBe('number');
+          expect(result._id).toBe(1010001);
+          expect(typeof result.email).toBe('string');
+          expect(result.email).toBe('test@test.test');
+          expect(typeof result.password).toBe('string');
+          expect(result.password).toBe('password12345');
+        });
+      });
   });
-  it('should close the connection', (done) => {
-    expect.hasAssertions();
-    endStub.callsFake(() => {
-      expect(endStub.called).toBe(true);
-      console.log('connection closed');
-      done();
+  // get all data by keyword
+});
+
+describe('getDataByKeyword(user,_id, 1010001 )', () => {
+  // get all data
+  it('should throw error if data is null', () => {
+    // en vez de llamar .query llama al callFake
+    queryStub.callsFake((query, value, cb) => {
+      const result = userTable.filter((user) => user._id === value);
+      cb(error, result);
     });
-    db.closeConnection();
+    return db.getDataByKeyword('users', '_id', 1010002)
+      .catch((error) => {
+        expect(error.message).toBe('error');
+      });
+  });
+
+  it('should return data if exits from users', () => {
+    // en vez de llamar .query llama al callFake
+    queryStub.callsFake((query, value, cb) => {
+      const result = userTable.filter((user) => user._id === value);
+      cb(error, result);
+    });
+    return db.getDataByKeyword('users', '_id', 1010001)
+      .then((result) => {
+        expect(typeof result[0]._id).toBe('number');
+        expect(result[0]._id).toBe(1010001);
+      });
+  });
+});
+
+describe('postData( users, toInsert)', () => {
+  // get all data
+  it('should throw error if data is null', () => {
+    // en vez de llamar .query llama al callFake
+    queryStub.callsFake((query, toInsert, cb) => {
+      const result = userTable.push(toInsert);
+      cb(error, result);
+    });
+    return db.postData('users', '_id', 1010002)
+      .catch((error) => {
+        expect(error.message).toBe(undefined);
+      });
+  });
+
+  it('should return data if exits from users', () => {
+    // en vez de llamar .query llama al callFake
+    queryStub.callsFake((query, toInsert, cb) => {
+      const result = userTable.push(toInsert);
+      cb(error, result);
+    });
+    const newUser = {
+      _id: 1010002,
+      email: 'newuser@test.test',
+      password: 'password12345',
+    }
+    return db.postData('users', newUser)
+      .then((result) => {
+        console.log(result);
+      });
   });
 });
--- db-data/bq_data.js
+++ db-data/bq_data.js
@@ -1,14 +1,27 @@
+/* eslint-disable no-unused-vars */
 const mysql = require('mysql');
 const config = require('../config');
 
 const { dbUrl } = config;
-// eslint-disable-next-line no-console
-console.log(dbUrl);
 const pool = mysql.createConnection(dbUrl);
 // pool.connect();
 pool.connect((err) => {
   if (err) throw err;
 });
-// eslint-disable-next-line no-console
-console.log(pool.state);
+const createTable = (table, values) => {
+  const sql = `CREATE TABLE IF NOT EXISTS ${table} ${values}`;
+  pool.query(sql, (err, result) => {
+    if (err) throw err;
+  });
+};
+
+const userValues = '(_id int NOT NULL AUTO_INCREMENT,email VARCHAR(30), address VARCHAR(255), `password` text, rolesAdmin boolean, PRIMARY KEY (_id))';
+const productsValues = '(_id INTEGER NOT NULL AUTO_INCREMENT, name varchar(50), price float(2), image text, type varchar(50), dateEntry date, primary key (_id))';
+const ordersProductsValues = '(orderId integer, qty integer, productId INTEGER)';
+const ordersValues = '(_id INTEGER NOT NULL AUTO_INCREMENT, userId INTEGER NOT NULL, client varchar(20), status varchar(20), dateEntry date, dateProcessed date, PRIMARY KEY (_id))';
+createTable('users', userValues);
+createTable('products', productsValues);
+createTable('orders', ordersValues);
+createTable('orders_products', ordersProductsValues);
+
 module.exports = pool;
