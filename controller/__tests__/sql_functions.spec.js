0b8fa55c666307cd624acd50a61a10ddd3bcbc9b
paula113 <paulapierina96@gmail.com>
2020-08-10 17:14:55 -0500

    unit testing with sinon

--- controller/__tests__/sql_functions.spec.js
+++ controller/__tests__/sql_functions.spec.js
@@ -1,31 +1,78 @@
-/* eslint-disable max-len */
-// const { getAllData, getDataByKeyword, postData } = require('../../db-data/sql_functions');
-/* describe('Delete data', () => {
-  it('deberia eliminar ', async () => {
-    expect(
-      await db.query(sql`SELECT id, name FROM users WHERE name=${'Joe'}`),
-    ).toEqual(
-      [{ id: expect.any(Number), name: 'Joe' }],
-    );
-  });
-}); */
+// Importar sinon
+const sinon = require('sinon');
+// Mockear MySQL con sinon
+const mockMysql = sinon.mock(require('mysql'));
+// Crear stub para connect
+const connectStub = sinon.stub().callsFake((cb) => {
+  console.log('connected');
+  console.log(cb);
+  // cb();
+});
+// Crear stub para query
+const queryStub = sinon.stub();
+// Crear stub para end
+const endStub = sinon.stub();
+// Cuando se cree la conexión, reemplazar resultado con stubs
+mockMysql.expects('createConnection').returns({
+  connect: connectStub,
+  query: queryStub,
+  end: endStub,
+});
+// Invocar nuestra libreria/metodos a probar
+const db = require('../../db-data/sql_functions');
 
-/* describe('Quering Sql Functions', () => {
-  it('Should return all data from a table ', (done) => getAllData('users')
-    .then((result) => {
-      expect(Array.isArray(result)).toBe(true);
+describe('mySQL', () => {
+  /*
+  it('should create a db', (done) => {
+    // En cada test podemos reemplazar la funcion a ser llamada
+    // usando callsFake
+    expect.hasAssertions();
+    queryStub.callsFake((query, cb) => {
+      expect(query).toBe('CREATE DATABASE mydb');
+      // Este callback es el que se espera sea llamado en cada query
+      // aqui podemos retornar cualquier información que deseamos
+      cb();
       done();
-    }));
- // eslint-disable-next-line max-len
- it('Should return data when a specific condition is true ', (done) => getDataByKeyword('users', 'email', 'admin@host.host')
-    .then((result) => {
-      expect(Array.isArray(result)).toBe(true);
-      //   expect(result).toBe([{ id: expect.any(Number), email: 'admin@host.host' }]);
+    });
+    db.createDb();
+  }); */
+  /*
+  it('should create a table', (done) => {
+    expect.hasAssertions();
+    queryStub.callsFake((query, cb) => {
+      expect(query).toBe('CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))');
+      cb();
       done();
-    }));
-  it('Should add a record to a table', (done) => postData('users', { email: 'newUser@host.host' })
-    .then((result) => {
-      expect(typeof result.insertId).toBe('number');
+    });
+    db.createTable();
+  }); */
+  it('should insert into a table', (done) => {
+    expect.hasAssertions();
+    queryStub.callsFake((query, cb) => {
+      console.log(query);
+      console.log(cb);
+      expect(query).toBe('INSERT INTO users SET ?');
+      // cb();
       done();
-    }));
-}); */
+    });
+    db.postData('users', { email: 'test@cucaracha.test', password: 123456, rolesAdmin: false });
+  });
+  it('should select from a table', (done) => {
+    expect.hasAssertions();
+    queryStub.callsFake((query, cb) => {
+      expect(query).toBe('SELECT * FROM products');
+      cb(null, [{ email: 'test@cucaracha.test', password: 123456, rolesAdmin: false }]);
+      done();
+    });
+    db.getAllData('products');
+  });
+  // it('should close the connection', (done) => {
+  //   expect.hasAssertions();
+  //   endStub.callsFake(() => {
+  //     expect(endStub.called).toBe(true);
+  //     console.log('connection closed');
+  //     done();
+  //   });
+  //   db.closeConnection();
+  // });
+});
